cf1f1a1ae96db9dd680f7609b632b58c76770ed4
tecillam <teci_04@hotmail.com>
2025-06-29 15:29:58 +0200

    Test over component added

--- src/app/traffic-light/traffic-light.component.spec.ts
+++ src/app/traffic-light/traffic-light.component.spec.ts
@@ -1,23 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TrafficLightService } from '../traffic-light-service.service';
 import { TrafficLightComponent } from './traffic-light.component';
+import { of } from 'rxjs';
 
 describe('TrafficLightComponent', () => {
   let component: TrafficLightComponent;
   let fixture: ComponentFixture<TrafficLightComponent>;
+  let mockTrafficLightService: jasmine.SpyObj<TrafficLightService>;
 
   beforeEach(async () => {
+    mockTrafficLightService = jasmine.createSpyObj('TrafficLightService', ['simulateRoadTraffic']);
+    mockTrafficLightService.simulateRoadTraffic.and.returnValue(['initial_mock_road', 'step1_mock_road']);
+
     await TestBed.configureTestingModule({
-      imports: [TrafficLightComponent]
-    })
-    .compileComponents();
+      imports: [TrafficLightComponent],
+      providers: [{ provide: TrafficLightService, useValue: mockTrafficLightService }]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TrafficLightComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  xit('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('Should test service consumption', () => {
+    expect(mockTrafficLightService.simulateRoadTraffic).toHaveBeenCalled();
+  })
+
+  
 });
